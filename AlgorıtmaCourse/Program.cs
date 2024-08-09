d11e0def820d9ef1a32ebe6a7b9ba165181ebbc4
azize dursun <azizecakir16@gmail.com>
2024-08-09 15:20:34 +0300

    Proje dosyası ekle.

---
+++ AlgorıtmaCourse/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BTKAkademi.ProgramlamaTemelleri
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+      
+            Console.ReadKey();
+        }
+    }
+}
